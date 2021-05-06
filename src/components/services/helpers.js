23bb075b3e3dcb6565ae58519330e40ce59e725d
abdul.wadood@targetofs.com <abdul.wadood@targetofs.com>
2021-05-07 01:00:51 +0500

    Services Section Updated.

---
+++ src/components/services/helpers.js
@@ -0,0 +1,99 @@
+import jeep from "./../../images/jeep-service.jpg";
+
+import Paragliding from "./../../images/Paragliding.jpeg";
+
+import DirtBike from "./../../images/DirtBike.jpeg";
+import ATVQuad from "./../../images/ATVQuad.jpeg";
+import StudyTrip from "./../../images/study-trip.jpg";
+import CustomizedTrip from "./../../images/customized-trip.jpg";
+import HoneymoonTrip from "./../../images/honeymoon-trip.jpg";
+import Camping from "./../../images/camping.jpg";
+import CorporateTrip from "./../../images/corporate-tour.jpg";
+import CandleLightDinner from "./../../images/desertcandlelightdinner.jpg";
+import Hiking from "./../../images/hiking.jpg";
+
+
+
+const CardsItems = [
+  {
+    type: "activities",
+    img: jeep,
+    title: "Jeep Drive In Sarfaranga Dessert",
+    detail: `Sarfaranga Cold Desert Skardu contains large sand dunes that are sometimes covered in snow during winter. Situated at an elevation of 2,226 metres (7,303 ft) above sea level, the Cold Desert is one of the highest deserts in the world. Baltistan Adventures offers jeep safari for tourist to explore the beauty of Sarfaranga cold desert.`,
+  },
+  {
+    type: "activities",
+    img: ATVQuad,
+    title: "ATV Quad Drive In Sarfaranga Dessert",
+    detail: `ATV is a vehicle that travels on low-pressure tires, with a seat that is straddled by the operator, along with handlebars for steering control. It is designed to handle a wider variety of terrain than most other vehicles. Baltistan Adventures brings you the facility to enjoy this ride at Sarfaranga Cold Desert Skardu. Quad bikes are available on self drive so that the rider can enjoy a thrilling dessert safari of Sarfaranga Cold Desert.`,
+  },
+  {
+    type: "activities",
+    img: Paragliding,
+    title: "Paramotorgliding In Himaliya Mountains",
+    detail: `Paramount gliding, also called a motorized parachute or paraplane, is a type of aircraft that consists of a parafoil with a motor and wheels. The aircraft's airspeed is toperate safely at heights ranging from. Paramotor Gliding operate safely at heights ranging between 500 and 1500 feet above ground level.`,
+  },
+  {
+    type: "activities",
+    img: DirtBike,
+    title: "Dirt Bike In Sarfaranga",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "trips",
+    img: HoneymoonTrip,
+    title: "HoneyMoon Trip",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "trips",
+    img: StudyTrip,
+    title: "Students Trip",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "trips",
+    img: CustomizedTrip,
+    title: "Customized Trip",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "trips",
+    img: CorporateTrip,
+    title: "Corporate Trip",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "others",
+    img: Camping,
+    title: "Camping",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "others",
+    img: CandleLightDinner,
+    title: "Desert Candle Light Dinner",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+  {
+    type: "others",
+    img: Hiking,
+    title: "Hiking",
+    detail: `Dirt bikes are specially designed for off-road events. Compared to road-going motorcycles, off-road motor cycles are lighter and more flexible, having long suspension travel and high ground clearance.
+       It is a dream of every biker to fell the excitement of driving a dirt bike for a dessert safari. So Baltistan Adventures is offering the riders to enjoy dirt bike dessert safari at Sarfaranga Cold Desert Skardu`,
+  },
+
+ 
+
+
+  
+];
+
+export default CardsItems;
--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,5 +1,5 @@
-import React from "react-router";
-import { Col, Typography } from "antd";
+import React, { useState, useEffect } from "react";
+import { Col, Typography, Tabs } from "antd";
 import CardContainer from "./card";
 import jeep from "./../../images/jeep-service.jpg";
 
@@ -7,101 +7,57 @@ import Paragliding from "./../../images/Paragliding.jpeg";
 
 import DirtBike from "./../../images/DirtBike.jpeg";
 import ATVQuad from "./../../images/ATVQuad.jpeg";
-
 import "./styles.scss";
+
+import serviceCards from "./helpers";
+const { TabPane } = Tabs;
 
 const { Title, Paragraph } = Typography;
 
 export const Services = () => {
+  const [selCardType, setSelCardType] = useState("activities");
+
+  useEffect(() => {
+    console.log(selCardType);
+  });
+
+  const renderCardItem = (item) => {
+    return (
+      <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+        <CardContainer
+          title={item.title}
+          imgSrc={item.img}
+          descritpion={item.detail}
+        />
+      </Col>
+    );
+  };
   return (
     <div className="services-container">
-      {/* <img
-        src={ServiceImage}
-        className="main-image"
-        // style={{ width: "100%", height: "400px", objectFir: "contain" }}
-        alt="services"
-      /> */}
       <div className="main-heading">
         <Title>{`Explore The World`}</Title>
       </div>
       <div className="details-text">
         <Paragraph>{`Come Explore the world with us and enjoy the best trip of your life. We offer Trips to every end of the world.`}</Paragraph>
       </div>
-
-      {/* <div className="label">
-        <h1>Our Services</h1>
-        <br />
-        <h3>We have Some Exciting Offfers for you</h3>
-        <h3>So What are you waiting for</h3>
-      </div> */}
-      {/* <Row gutter={[16, 20]} type={"flex"}> */}
-      <div className="card-container">
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <CardContainer
-            title={"Jeep Drive In Sarfaranga Dessert"}
-            imgSrc={jeep}
-            descritpion={`Sarfaranga Cold Desert Skardu contains large sand dunes that are sometimes covered in snow
-            during winter. Situated at an elevation of 2,226 metres (7,303 ft) above sea level, the Cold
-            Desert is one of the highest deserts in the world.
-            
-            Baltistan Adventures offers jeep safari for tourist to explore the beauty of
-Sarfaranga cold desert.`}
-          />
-        </Col>
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <CardContainer
-            title={"ATV Quad Drive In Sarfaranga Dessert"}
-            imgSrc={ATVQuad}
-            descritpion={`ATV is a vehicle that travels on low-pressure tires, with a seat that is straddled by the operator,
-            along with handlebars for steering control. It is designed to handle a wider variety of terrain
-            than most other vehicles.
-            
-            Baltistan Adventures brings you the facility to enjoy this ride at Sarfaranga Cold
-Desert Skardu. Quad bikes are available on self drive so that the rider can enjoy a
-thrilling dessert safari of Sarfaranga Cold Desert.`}
-          />
-        </Col>
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <CardContainer
-            title={"Paramotorgliding In Himaliya Mountains"}
-            imgSrc={Paragliding}
-            descritpion={
-              <div>
-                Paramount gliding, also called a motorized parachute or
-                paraplane, is a type of aircraft that consists of a parafoil
-                with a motor and wheels. The aircraft's airspeed is toperate
-                safely at heights ranging from. Paramotor Gliding operate safely
-                at heights ranging between 500 and 1500 feet above ground level.
-                {/* <br />A trained pilot will operate the glider and a rider can
-                enjoy the flight on a seat next to the pilot. Paramotor ride is
-                a safe and excitement activity. Now all the tourist can enjoy
-                the thrilling ride of Paramotor and spectate airial view of
-                Skardu under the services of Baltistan Adventures. */}
-              </div>
-            }
-          />
-        </Col>
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <CardContainer
-            title={"Dirt Bike In Sarfaranga"}
-            imgSrc={DirtBike}
-            descritpion={
-              <div>
-                Dirt bikes are specially designed for off-road events. Compared
-                to road-going motorcycles, off-road motor cycles are lighter and
-                more flexible, having long suspension travel and high ground
-                clearance.
-                <br />
-                It is a dream of every biker to fell the excitement of driving a
-                dirt bike for a dessert safari. So Baltistan Adventures is
-                offering the riders to enjoy dirt bike dessert safari at
-                Sarfaranga Cold Desert Skardu
-              </div>
-            }
-          />
-        </Col>
+      <div className="services--tabs-container">
+        <Tabs
+          type="card"
+          onChange={(key) => {
+            setSelCardType(key);
+          }}
+        >
+          <TabPane tab="Activities" key="activities" />
+          <TabPane tab="Trips" key="trips" />
+          <TabPane tab="Others" key="others" />
+        </Tabs>
       </div>
-      {/* </Row> */}
+
+      <div className="card-container">
+        {serviceCards
+          .filter((item) => item.type === selCardType)
+          .map((item) => renderCardItem(item))}
+      </div>
     </div>
   );
 };

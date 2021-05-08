d352966e12a31d00b93874a34d3e0ff319f4e33d
abdul.wadood@targetofs.com <abdul.wadood@targetofs.com>
2021-05-09 03:49:31 +0500

    Dummy Animation removed.

--- src/components/about-us/index.js
+++ src/components/about-us/index.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
-import OnScreen from "./../isInViewport";
+import React, {  useRef } from "react";
+// import OnScreen from "./../isInViewport";
 import { Typography } from "antd";
 import "./styles.scss";
 const { Title, Text } = Typography;
 
 export const AboutUs = () => {
-  const [visible, setVisible] = useState(false);
+  // const [visible, setVisible] = useState(false);
   const ref = useRef();
-  var isVisible = OnScreen(ref);
+  // var isVisible = OnScreen(ref);
 
-  useEffect(() => {
-    setVisible(isVisible);
-  }, [isVisible]);
+  // useEffect(() => {
+  //   setVisible(isVisible);
+  // }, [isVisible]);
   return (
     <div className="about-us-container">
       <div className={`main-heading`} ref={ref}>
         <Title>{`About Us`}</Title>
         
       </div>
-      <div className={`details-text ${visible && "slide-from-left"}`}>
+      <div className={`details-text`}>
         <Text>
           <Text strong>{`BALTISTAN ADVENTURES`}</Text>
           {` is an adventure tourism based company based in Skardu, GilgitBaltistan. Our mission is to promote adventure tourism in Gilgit-Baltistan. We believe that apart

9f4980bb775435f34bf8865d71345b0c29ddc9c4
abdul.wadood@targetofs.com <abdul.wadood@targetofs.com>
2021-05-10 12:37:26 +0500

    IMage size changed.

--- src/components/galley/index.js
+++ src/components/galley/index.js
@@ -20,7 +20,7 @@ ATV quad bikes, Dirt bikes desert jeep safari and other exciting activities in S
 
       <div className="images-container">
           
-      {Images.map(item=><Col xs={10} sm={10} md={8} lg={8} xl={6}>
+      {Images.map(item=><Col xs={12} sm={12} md={8} lg={8} xl={6}>
           <Image src={item.url} />
       </Col>)}
       </div>
